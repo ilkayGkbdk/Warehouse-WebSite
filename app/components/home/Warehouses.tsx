84c03f196d7d461ae61531a3509430986b8a9e48
ilkayGkbdk <ilkayrg55@hotmail.com>
2024-12-07 02:57:14 +0300

    navbar, home and footer basics

---
+++ app/components/home/Warehouses.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/general/Header";
+
+const Products = () => {
+    return (
+        <div>
+            <Header text='Tüm Depolar' center/>
+            <div className='flex items-center flex-wrap gap-3 md:gap-10 px-3 md:px-10'>
+                TODO
+            </div>
+        </div>
+    );
+}
+
+export default Products;
---
+++ app/components/navbar/WarehouseCount.tsx
@@ -0,0 +1,9 @@
+const CartCount = () => {
+    return (
+        <div className='hidden md:flex items-center justify-center'>
+            CartCount
+        </div>
+    );
+}
+
+export default CartCount;
